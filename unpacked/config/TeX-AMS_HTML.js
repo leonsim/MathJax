ed52df482800e89b9f173e2aaabf00e4764b4c56
Liang Sun <i@liangsun.org>
2014-04-02 15:50:38 +0800

    Add overparen macro.

--- unpacked/config/TeX-AMS_HTML.js
+++ unpacked/config/TeX-AMS_HTML.js
@@ -17,6 +17,14 @@
  */
 
 MathJax.Hub.Config({
+    TeX: { Augment: {
+      Definitions: {
+        macros: {
+          overparen: ['UnderOver','23DC'], // UnderOver represents the MathML element, 23DC the unicode character
+          underparen: ['UnderOver','23DD'] // UnderOver represents the MathML element, 23DC the unicode character
+        }
+      }
+    }},
   extensions: ["tex2jax.js","MathEvents.js","MathZoom.js","MathMenu.js","toMathML.js","TeX/noErrors.js","TeX/noUndefined.js","TeX/AMSmath.js","TeX/AMSsymbols.js"],
   jax: ["input/TeX","output/HTML-CSS"]
 });
